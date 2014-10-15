5f1f34d566ec4cedc532d2035178321ad638ec14
Brandan Haertel <foilpudding@gmail.com>
2014-10-15 09:49:48 -0700

    added tower place snap and logic so 2 towers cannot occupy 1 space

--- BeachTD-Prototypes/Assets/Resources/GameManager.cs
+++ BeachTD-Prototypes/Assets/Resources/GameManager.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
+	private float SnapGrid = .5f;
+	private Vector3 BadVector = new Vector3(6,6,6);
+
 	// Use this for initialization
 	void Start () {
 	
@@ -28,28 +31,50 @@ public class GameManager : MonoBehaviour {
 			
 		Debug.Log("Create Tower");
 		
-		GameObject e = Instantiate(towerPrefab) as GameObject;
-		TowerBehavior spawnedTower = e.GetComponent<TowerBehavior>();
-		if(spawnedTower != null) {
-			Vector3 newPos = getPos(pos);
-			e.transform.position = newPos;
+		Vector3 newPos = getPos(pos);
+		if(newPos != BadVector)
+		{
+			GameObject e = Instantiate(towerPrefab) as GameObject;
+			TowerBehavior spawnedTower = e.GetComponent<TowerBehavior>();
+			if(spawnedTower != null) {
+				e.transform.position = newPos;
+			}
 		}
+			
 	}
 	
+	// snap tower position to .5 unit grid
 	private Vector3 getPos(Vector3 v)
 	{
-		if(v.x % .5 != 0)
-		{
-			// x is not a multiple of .5
-			Debug.Log("x not aligned");
-			
+		Vector3 p = new Vector3(v.x, v.y, 0f);
+		
+		// round x to nearest snap point
+		if((v.x % SnapGrid) < (SnapGrid/2)){ // round x down
+			p.x -= (v.x % SnapGrid);
+		}
+		else if((v.x % SnapGrid) < SnapGrid){ // round x up
+			p.x += (SnapGrid - (v.x % SnapGrid));
 		}
 		
-		if(v.y % .5 != 0)
-		{
-			// y is not a multiple of .5
-			Debug.Log("y not aligned");
-			
+		// round y to nearest snap point
+		if((v.y % SnapGrid) < (SnapGrid/2)){ // round y down
+			p.y -= (v.y % SnapGrid);
 		}
+		else if((v.y % SnapGrid) < SnapGrid){ // round y up
+			p.y += (SnapGrid - (v.y % SnapGrid));
+		}
+		
+		Vector2 p2 = new Vector2(p.x, p.y);
+		
+		RaycastHit2D hit = Physics2D.Raycast(p, Vector2.zero);
+		if(hit.collider == null)
+			Debug.Log("no colliders");
+		else if(hit != null && hit.collider.tag == "tower")
+		{
+			Debug.Log("Found Tower where building");
+			return BadVector;
+		}
+		
+		return p;
 	}
 }
