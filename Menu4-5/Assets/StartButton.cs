e91f0721fb10d1a8c5e7bb997a1f3e23c57568e5
Brandan Haertel <foilpudding@gmail.com>
2014-10-16 23:07:09 -0700

    made Unity4.5 Gui test, Menu4-5, used GUITextures, works well

--- Menu4-5/Assets/StartButton.cs
+++ Menu4-5/Assets/StartButton.cs
@@ -2,26 +2,96 @@
 using System.Collections;
 
 public class StartButton : MonoBehaviour {
-
-	private GUITexture myGuiTextObject;
-	
-	// Use this for initialization
-	void Start () {
-		myGuiTextObject = GameObject.Find("StartButton") as GUITexture;
-	}
 	
 	// Update is called once per frame
 	void Update () {
 	
-		Touch touchObj = Input.GetTouch(0);
-		
-		if (touchObj.phase == Input.Began)
+		if(Input.touchCount > 0)
 		{
-			if(myGuiTextObject.HitTest(touchObj.position))
+			if(guiTexture.HitTest(Input.GetTouch(0).position))
 			{
-				Debug.Log ("Startbutton");
+				// send game object OnTouchDown Message
+				// do not require the Object to have the method "OnTouchDown"
+				if(Input.GetTouch(0).phase == TouchPhase.Began)
+				{
+					this.SendMessage("OnTouchDown", Input.GetTouch(0).position, SendMessageOptions.DontRequireReceiver);
+				}
+				
+				// send game object OnTouchUp Message
+				// do not require the Object to have the method "OnTouchUp"
+				if(Input.GetTouch(0).phase == TouchPhase.Ended)
+				{
+					this.SendMessage("OnTouchUp", Input.GetTouch(0).position, SendMessageOptions.DontRequireReceiver);
+				}
+				
+				// send game object OnTouchStay Message
+				// do not require the Object to have the method "OnTouchStay"
+				if(Input.GetTouch(0).phase == TouchPhase.Stationary)
+				{
+					this.SendMessage("OnTouchStay", Input.GetTouch(0).position, SendMessageOptions.DontRequireReceiver);
+				}
+				
+				// send game object OnTouchMove Message
+				// do not require the Object to have the method "OnTouchMove"
+				if(Input.GetTouch(0).phase == TouchPhase.Moved)
+				{
+					this.SendMessage("OnTouchMove", Input.GetTouch(0).position, SendMessageOptions.DontRequireReceiver);
+				}
+				
+				// send game object OnTouchExit Message
+				// do not require the Object to have the method "OnTouchExit"
+				if(Input.GetTouch(0).phase == TouchPhase.Canceled)
+				{
+					this.SendMessage("OnTouchExit", Input.GetTouch(0).position, SendMessageOptions.DontRequireReceiver);
+				}
 			}
 		}
 		
 	}
+	
+	// Touch Began
+	// called when the object is first pressed
+	public void OnTouchDown()
+	{	
+		// Load level manager that has int keys for levels
+		//LevelManager lvl = new LevelManager();
+		
+		this.transform.position = new Vector3(
+			this.transform.position.x + .1f,
+			this.transform.position.y,
+			this.transform.position.z
+		);
+		
+		// Load scene
+		//Application.LoadLevel(lvl.getLevel1());
+	}
+	
+	// Touch Ended
+	// called when the object is stopped being touched
+	public void OnTouchUp()
+	{
+	
+	}
+	
+	// Touch Stationary
+	// called when the object is held
+	public void OnTouchStay()
+	{
+	
+	}
+	
+	// Touch Moved
+	// called when the touch is moving on the object
+	public void OnTouchMoved()
+	{
+	
+	}
+	
+	// Touch Canceled
+	// called when the touch moved off the object
+	public void OnTouchExit()
+	{
+	
+	}
+	
 }
