7c3436978ca95120672fddda24a3d3f8d242dc15
Brandan Haertel <foilpudding@gmail.com>
2014-10-16 17:25:01 -0700

    worked on MenuGui

---
+++ Menu4-5/Assets/StartButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartButton : MonoBehaviour {
+
+	private GUITexture myGuiTextObject;
+	
+	// Use this for initialization
+	void Start () {
+		myGuiTextObject = GameObject.Find("StartButton") as GUITexture;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		Touch touchObj = Input.GetTouch(0);
+		
+		if (touchObj.phase == Input.Began)
+		{
+			if(myGuiTextObject.HitTest(touchObj.position))
+			{
+				Debug.Log ("Startbutton");
+			}
+		}
+		
+	}
+}
