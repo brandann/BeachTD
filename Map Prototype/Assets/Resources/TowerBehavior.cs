a8165873973263066d780c37cd85e097516f3c7d
Brandan Haertel <foilpudding@gmail.com>
2014-11-02 08:41:21 -0800

    started work on TowerManager, lay out of functions

--- Map Prototype/Assets/Resources/TowerBehavior.cs
+++ Map Prototype/Assets/Resources/TowerBehavior.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class TowerBehavior : MonoBehaviour {
 
+	public GameObject[] TowerUpgrades;
+	public GameObject TowerIcon;
+	public int Cost;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -14,6 +18,8 @@ public class TowerBehavior : MonoBehaviour {
 	}
 	
 	// Destroy myself
+	// a simple destroy method that finishes any tasks that
+	// need to be done before the tower is completly gone.
 	public void destroy()
 	{
 		Destroy(this.gameObject);
@@ -21,9 +27,39 @@ public class TowerBehavior : MonoBehaviour {
 	
 	void OnTouchDown()
 	{
+		// will instantiate a new tower manager that will create a
+		// menu system to: upgrade, sell, or build towers.
+		// pass this GameObject to manager to provide manager with a refrence to
+		// this for Destroying and replacing
 		GameObject towerPrefab = Resources.Load("Prefabs/OpenArea") as GameObject;
 		GameObject e = Instantiate(towerPrefab) as GameObject;
 		e.transform.position = this.transform.position;
 		Destroy(this.gameObject);
+	}
+	
+	// returns an array of available upgrades for this tower
+	// used to display to the manager what upgrades are available
+	// for a tower
+	public GameObject[] getUpgrades()
+	{
+		return TowerUpgrades;
+	}
+	
+	// returns an icon for this tower
+	// icon knows this tower and can hold the GameObject information
+	// for the manager
+	public GameObject getTowerIcon()
+	{
+		return TowerIcon;
+	}
+	
+	// returns the cost of the tower
+	// this is the full cost that the tower costs,
+	// can be negative if the player gets a return, or positive
+	// if the player needs to pay, or zero if the tower has no impact on the
+	// players "wallet"
+	public int getCost()
+	{
+		return Cost;
 	}
 }
---
+++ Map Prototype/Assets/Resources/TowerManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TowerManager : MonoBehaviour {
+
+	GameObject orgTower;
+	GameObject upgrade;
+	GameObject btn1;
+	GameObject btn2;
+	GameObject btn3;
+	GameObject btn4;
+	
+	public GameObject towerAIcon;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	//
+	public void setOrgTower(GameObject go)
+	{
+		orgTower = go;
+	}
+	
+	public void setUpgradeTower(GameObject go)
+	{
+	
+	}
+	
+	private GameObject setButton(GameObject go, Vector3 pos)
+	{
+		return null;
+	}
+	
+	public void Confirm(GameObject go)
+	{
+	
+	}
+	
+	
+}
