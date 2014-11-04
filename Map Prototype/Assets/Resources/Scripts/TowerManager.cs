3ced4bc9cf0dc14fa9aecaeda4740c8cc5556484
Brandan Haertel <foilpudding@gmail.com>
2014-11-04 10:06:38 -0800

    updated TowerManager.cs comments

--- Map Prototype/Assets/Resources/Scripts/TowerManager.cs
+++ Map Prototype/Assets/Resources/Scripts/TowerManager.cs
@@ -3,8 +3,14 @@ using System.Collections;
 
 public class TowerManager : MonoBehaviour {
 
+  // hold the instance of the "touched" tower
 	GameObject orgTower;
+  
+  // hold the instance of the new tower
 	GameObject upgrade;
+  
+  // instances of TowerIcons
+  // used for Destroy at end of Manager
 	GameObject btn1;
 	GameObject btn2;
 	GameObject btn3;
@@ -22,22 +28,31 @@ public class TowerManager : MonoBehaviour {
 	
 	}
 	
-	//
+	// set orgTower to the original tower that was touched
+  // this is the tower to be replaced at end of manager cycle
+  // can be Tower or OpenSpace
 	public void setOrgTower(GameObject go)
 	{
 		orgTower = go;
 	}
 	
+  // setUpgradeTower to the tower to be built
+  // go is passed in with then TowerIcon when touched
+  // can be Tower or OpenSpace
 	public void setUpgradeTower(GameObject go)
 	{
 	
 	}
 	
+  // generic method to set TowerIcons around the selected tower
 	private GameObject setButton(GameObject go, Vector3 pos)
 	{
 		return null;
 	}
 	
+  // Confirm commits the change from orgTower to UpgradeTower
+  // can be Tower or OpenSpace
+  // double check notes for use of 'go'
 	public void Confirm(GameObject go)
 	{
 	
