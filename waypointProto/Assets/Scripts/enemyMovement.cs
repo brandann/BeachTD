e82b695fd7e3aa6481664f368df4245d4b7cdc2d
erickhouse1 <erickhouse01@gmail.com>
2014-11-07 16:37:55 -0800

    added my stuff

--- waypointProto/Assets/Scripts/enemyMovement.cs
+++ waypointProto/Assets/Scripts/enemyMovement.cs
@@ -5,10 +5,11 @@ public class enemyMovement : MonoBehaviour {
 
 	public GameObject[] waypoints;
 
-	private Vector3 nextPoint;
-	private Vector3 currentPoint;
-	private float speed = 3;
+	private Vector3 nextPoint; // the next waypoint the enemy is traveling to
+	private float speed = 2;
 	private int listPos = 1; //current index of the array list
+	bool right = false; //should the enemy rotate right?
+	bool left = false; //should the enemy rotate left?
 
 	private ArrayList waypointList;
 
@@ -27,32 +28,69 @@ public class enemyMovement : MonoBehaviour {
 			transform.up = nextPoint - transform.position; //gets the first direction
 		}
 	}
-	
-	// Update is called once per frame
+
 	void Update ()
 	{
+		float dot = 0;
+		Vector3 currentPos = nextPoint - transform.position; //how close am I to the next waypoint? 
 
-		Vector3 currentPos = nextPoint - transform.position;
+		if (currentPos != transform.up)// no need to rotate, object is moving in a straight line
+		{
+			 dot = transform.up.x * -currentPos.y + transform.up.y * currentPos.x; //how much more do I have to rotate? 
+		}
 
 		//once enemy gets really close to the waypoint,
 		// go to the next waypoint until there are none left, then reverse
-		if (currentPos.magnitude < .05 && listPos < waypointList.Count-1) 
+		if (currentPos.magnitude < .5 && listPos < waypointList.Count - 1)
 		{
-		listPos++;
-			nextPoint = (Vector3)waypointList [listPos];
-			Vector3 dir = nextPoint - transform.position;
+				listPos++;
+				nextPoint = (Vector3)waypointList [listPos];
+				Vector3 dir = nextPoint - transform.position; // the new vector to turn towards
 
-			transform.up = dir;
+				// by making dir.y negative we flip the angle 90 degrees. By doing that we can easily use to dot product to determine
+				// right or left. Normally, the dot product returns a - or + angle based on front or behind
+				dot = transform.up.x * -dir.y + transform.up.y * dir.x;
+		
+				if (dot > 0) //my next waypoint is to the right
+				{
+					print ("b on the right of a");
+					right = true;
+				}
+				else if (dot < 0) // my next waypoint is to the left
+				{
+					left = true;
+					print ("b on the left of a");
+				}
+				else
+					print ("b parallel/antiparallel to a");
+		
 		} 
-		else if (listPos == waypointList.Count-1)
+		else if (listPos == waypointList.Count - 1) //I'm out of waypoints, turn around
 		{
-			waypointList.Reverse();
-			listPos = 0;
+				listPos = 0;
+				waypointList.Reverse ();
+		}
+
+				
+
+		if (right && dot < 0) { //I want to turn right as long as the dot calculation doesn't go over zero (which means the object has 
+								// rotated past the waypoint it needs to go to)
+			right = false;
+			transform.up = currentPos; //makes sure we dont rotate past our destination 
+		} else if (right) {
+			transform.Rotate (Vector3.forward, -1f * (120f * Time.smoothDeltaTime));
+		}
+
+
+		if (left && dot > 0) { //exactly the same as above, but inverted
+			left = false;
+			transform.up = currentPos;
+		} else if(left) {
+			transform.Rotate (Vector3.forward, 1f * (120f * Time.smoothDeltaTime));	
 		}
 
 		//movement operation
 		transform.position += (speed * Time.smoothDeltaTime) * transform.up;
-
 
 	}
 //----------------------------------------------------------------------
@@ -70,7 +108,7 @@ public class enemyMovement : MonoBehaviour {
 
 				 float temp = float.MaxValue; //the current shortest magnitude between vectors
 				 Vector3 tempV = new Vector3(); //the waypoint with the current shortest distance from "current"
-				 int currentIndex = 0; // the index of that waypoint in order to remove it from the array
+				 int currentIndex = 0; // the index of that waypoint, in order to remove it from the array
 
 				for(int j = 0; j < waypoints.Length; j++)
 				{
@@ -97,11 +135,6 @@ public class enemyMovement : MonoBehaviour {
 		
 			}
 
-//			foreach(Vector3 v in waypointList)
-//			{
-//				print("count" + waypointList.Count);
-//				print (v);
-//			}
 		}
 
 
