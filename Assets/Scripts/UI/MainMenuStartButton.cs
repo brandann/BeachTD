a075de978da92a64090b093a40ddd45a804f9021
prevnode <jgmixson@uw.edu>
2014-12-26 07:56:42 -0600

    Should have been more commits: Added script for main menu buttons, Setup animation for credits

---
+++ Assets/Scripts/UI/MainMenuStartButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuStartButton : SlidingButton{
+
+	public override void Clicked ()
+	{
+		base.Clicked ();
+	}
+
+	void OnBecameInvisible(){
+		Application.LoadLevel (1);
+	}
+	
+}
---
+++ Assets/Scripts/UI/SlidingButton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlidingButton : MonoBehaviour {
+
+	public Animator ButtonAnimController;
+	private int mSlideHash;
+	
+	void Start(){
+		
+		mSlideHash = Animator.StringToHash ("Slide");
+	}
+	
+	public void Slide(){
+		ButtonAnimController.SetTrigger (mSlideHash);
+	}
+
+	public virtual void Clicked(){
+		//Ignore multiple clicks while sliding out
+		if (ButtonAnimController.animation.isPlaying)
+						return;
+					
+		Slide ();
+	}
+}
