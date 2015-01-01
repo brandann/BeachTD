3758f98b70d5843ad18fbd7ef5b5987675210396
Brandan Haertel <foilpudding@gmail.com>
2014-12-31 18:17:55 -0800

    merged gui with master

---
+++ Assets/Resources/Scripts/UI/MainMenuCreditsButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuCreditsButton : SlidingUI {
+
+    public MainMenuStartButton StartButton;
+    public SlidingUI CreditsScript;
+
+    public override void Clicked()
+    {
+ 	    base.Clicked();
+        StartButton.Slide();
+        CreditsScript.Slide();
+    }
+
+}
---
+++ Assets/Resources/Scripts/UI/MainMenuStartButton.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuStartButton : SlidingUI{
+
+	public MainMenuCreditsButton Credits;
+    public AnimationClip SlideAnimation;
+    
+    private Camera mSceneCam;
+    private float mSlideTime;
+    
+
+    public override void Start()
+    {
+        base.Start();
+        mSceneCam = Camera.main;
+        mSlideTime = SlideAnimation.length;
+    }
+
+	public override void Clicked ()
+	{
+		base.Clicked ();
+		Credits.Slide ();
+        StartCoroutine(LoadLevel());
+	}
+
+    private IEnumerator LoadLevel()
+    {
+        yield return new WaitForSeconds(mSlideTime);
+        Application.LoadLevel(1);
+     
+    }
+	
+}
