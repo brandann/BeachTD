9b205487587b136e3d952affc5bc0ca6c700dea9
Gary <jgmixson@uw.edu>
2015-02-15 18:42:10 -0800

    Position GUI status bar elements

---
+++ Assets/Resources/Scripts/Tower/OpenAreaBehavior.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenAreaBehavior : MonoBehaviour {
+
+    public delegate void OpenAreaHandler(OpenAreaBehavior area);
+
+    public static event OpenAreaHandler onAreaTouched;
+
+    public void OnTouchDown()
+    {
+        if (onAreaTouched != null)
+        {
+            onAreaTouched(this);
+        }
+    }
+
+}
---
+++ Assets/StatusBar.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatusBar : MonoBehaviour {
+
+    public delegate void UpdateMoney(int money);
+
+    public static event UpdateMoney OnMoneyChanged;
+
+
+
+
+
+}
