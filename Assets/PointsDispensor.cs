7890d962ec17fc5406e5263a1f4dec3bc2eb186b
Gary <jgmixson@uw.edu>
2015-02-17 11:56:50 -0800

    Add points display when enemies die

---
+++ Assets/PointsDispensor.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class PointsDispensor : MonoBehaviour {
+
+    public GameObject PointsPrefab;
+
+    private Queue<GameObject> _points;
+    private int poolDepth = 10;
+
+	// Use this for initialization
+	void Start () {
+
+        _points = new Queue<GameObject>(poolDepth);
+
+        //Fill object pool
+        for (int i = 0; i < poolDepth; ++i)
+        {
+            GameObject points = Instantiate(PointsPrefab) as GameObject;
+            points.SetActive(false);
+            _points.Enqueue(points);
+        }
+	
+	}
+
+    void OnEnable()
+    {
+        Enemy.SomeEnemyDied += HandleEnemyDeath;
+    }
+
+    void OnDisable()
+    {
+        Enemy.SomeEnemyDied -= HandleEnemyDeath;
+    }
+	
+	private void HandleEnemyDeath(Enemy enemy)
+    {
+        GameObject points = _points.Dequeue();
+        points.SetActive(true);
+        points.transform.position = enemy.transform.position;
+        points.GetComponentInChildren<Text>().text = "+" + enemy.EnemyKillValue.ToString();
+        _points.Enqueue(points);
+
+    }
+}
