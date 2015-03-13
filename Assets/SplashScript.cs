e95099230ef2d24370c18a227751aaa16eef5734
Brandan Haertel <foilpudding@gmail.com>
2015-03-12 21:09:14 -0700

    roughed in Nybble Splash screen

---
+++ Assets/SplashScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScript : MonoBehaviour {
+
+	private float delay;
+	private float _spawntimedinterval;
+	
+	// Use this for initialization
+	void Start () {
+		delay = 3;
+		_spawntimedinterval = Time.realtimeSinceStartup;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((Time.realtimeSinceStartup - _spawntimedinterval) > delay)
+		{
+			Application.LoadLevel(Global.Scenes.Menu.ToString());
+		}
+	}
+}
