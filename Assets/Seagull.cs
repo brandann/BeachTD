3de00bedcfb770e6b4b28b33cbb807f88b7815c9
Gary <jgmixson@uw.edu>
2015-03-17 10:41:52 -0700

    Gull now finds and targets eggs

--- Assets/Seagull.cs
+++ Assets/Seagull.cs
@@ -3,23 +3,102 @@ using System.Collections;
 
 public class Seagull : MonoBehaviour {
 
-    public float Speed = 3;
+    public float Speed = 2;
 
+    //How long before gull tries for an egg
+    public float Delay = 1;
 
-	// Use this for initialization
+    #region MonoBehaviour
+
+    // Use this for initialization
 	void Start () {
-
-        GameObject go = GameObject.Find("egg");
-        if(go != null)
-            _target = go.transform.position;
-	
+        _lastAttack = Time.time;
+        Egg.EggPickedUp += EggGrabbed;
 	}
+
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+
         if( _target != null)
-            transform.position += (_target - transform.position) * Speed * Time.deltaTime;
+            transform.position += (_target.position - transform.position).normalized * Speed * Time.deltaTime;
+        else
+            if (Time.time >= _lastAttack + Delay)
+            {
+                _lastAttack = Time.time;
+                FindTargetEgg();
+            }
 	}
 
-    private Vector3 _target;
+    void OnDestroy()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "egg")
+        {
+            Egg hit = col.GetComponent<Egg>();
+
+            if (hit != _targetEgg)
+                return;
+
+            _targetEgg.Kill();
+
+            _target = null;
+            _targetEgg = null;
+        }
+
+    }
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+
+        if (col.gameObject.tag == "egg")
+        {
+            Egg hit = col.GetComponent<Egg>();
+
+            if (hit != _targetEgg)
+                return;
+
+            _targetEgg.Kill();
+
+            _target = null;
+            _targetEgg = null;
+        }
+
+    }
+
+    #endregion
+
+    private void FindTargetEgg()
+    {
+        Egg egg;
+        GameObject[] eggs = GameObject.FindGameObjectsWithTag("egg");        
+        for(int i = 0; i < eggs.Length; ++i){
+            egg = eggs[i].GetComponent<Egg>();
+
+            if (egg != null && !egg.BeingCarried)
+            {
+                _target = eggs[i].transform;
+                _targetEgg = egg;
+            }
+        }
+
+    }
+
+    private void EggGrabbed(Egg egg)
+    {
+        Egg target = _target.GetComponent<Egg>();
+        if(target == egg)
+        {
+            _target = null;
+            FindTargetEgg();
+        }
+    }
+
+    private Transform _target;
+    private Egg _targetEgg;
+    private float _lastAttack;
 }
