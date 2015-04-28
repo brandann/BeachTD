9e380951f3731f54e1af77ffe85965c89a7abbf7
Brandan Haertel <foilpudding@gmail.com>
2015-04-28 10:15:35 -0700

    fixed prototype prefabs

---
+++ Art-Prototypes/Art-Prototypes/Assets/Scripts/CopterMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CopterMovement : MonoBehaviour
+{
+
+    public float Speed; 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+        Vector3 lookPos = Camera.main.ScreenToWorldPoint(mousePos);
+        lookPos = lookPos - transform.position;
+        transform.up = lookPos;
+
+        transform.position += transform.up * Speed * Time.deltaTime;
+	}
+}
