21f4e2e2fbb3d4b63da33ddf2f6471c752fecfcf
Brandan Haertel <foilpudding@gmail.com>
2016-01-14 08:23:01 -0800

    updating some balancing and fixed the bottom panel

--- Assets/Resources/Scripts/Managers/EnemyManager.cs
+++ Assets/Resources/Scripts/Managers/EnemyManager.cs
@@ -15,7 +15,7 @@ public class EnemyManager : ManagerBase
 	private Queue<EnemySchedule> _enemyQueue;
 	
     private float delay;
-    private float delay_multiplyer = 4;
+    private float delay_multiplyer = 2;
     
     private GameObject EnemyA0Prefab;
     private GameObject EnemyB0Prefab;
