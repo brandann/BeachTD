8ce69d1d13677c05a226f957452c0ce0f65bf049
brandann <foilpudding@gmail.com>
2016-02-09 11:37:58 -0800

    working on balance

--- Assets/Resources/Scripts/Map/Maps/Map07.cs
+++ Assets/Resources/Scripts/Map/Maps/Map07.cs
@@ -5,12 +5,12 @@ public class Map07 : GameMap {
 	public override Map GetMap ()																													
 	{
         int[,] waves = new int[,] {
-            {A,A,A,A,A,0,0,0,0,0,0},
-            {A,A,A,A,B,B,0,0,0,0,0},
-            {A,A,A,A,A,A,A,A,A,A,0},
-            {B,A,A,A,A,A,B,0,0,0,0},
-            {A,A,A,A,A,A,A,A,A,A,C},
-            {A,A,A,A,A,A,A,A,A,A,C}
+{A,	A,	A,	A,	A,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0},
+{A,	A,	B,	B,	A,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0},
+{A,	A,	A,	A,	A,	A,	A,	A,	A,	A,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0},
+{B,	A,	A,	A,	A,	A,	B,	B,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0},
+{A,	A,	A,	A,	A,	A,	A,	A,	A,	A,	C,	0,	0,	0,	0,	0,	0,	0,	0,	0},
+{A,	A,	A,	A,	A,	A,	A,	A,	A,	A,	C,	0,	0,	0,	0,	0,	0,	0,	0,	0}
         };
 		
         int[,] intmap = new int[,] {
