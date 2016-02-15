9ef86778ed04241bff0853cc1a4277e2064f9f6f
brandan <foilpudding@gmail.com>
2016-02-15 14:26:50 -0800

    updated the menu UI

---
+++ Assets/Resources/Scripts/UI/OptionsButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OptionsButton : MonoBehaviour {
+
+    public GameObject Hide;
+    public GameObject Show;
+
+    public void SimpleClicked()
+    {
+        Show.SetActive(true);
+        Hide.SetActive(false);
+    }
+}
